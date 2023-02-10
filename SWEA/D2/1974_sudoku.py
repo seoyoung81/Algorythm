3c83e54604bb9e2f205dd585f21df414602f1ea3
seoyoung81 <seosue0801@gmail.com>
2023-02-10 17:54:08 +0900

    0210 study

--- SWEA/D2/1974_sudoku.py
+++ SWEA/D2/1974_sudoku.py
@@ -5,11 +5,12 @@ T = int(input())    # 테스트 케이스의 개수
 for tc in range(1, T+1):
     row_sudoku = [list(map(int, input().split())) for _ in range(9)]     # 스도쿠를 2차원 리스트로 입력 받음
     col_sudoku = [[0] * 9 for _ in range(9)]
-
+    # 세로 줄 탐색
     for i in range(9):
         for j in range(9):
             col_sudoku[i][j] = row_sudoku[j][i]
 
-    print(row_sudoku)
-    print(col_sudoku)
-
+    for i in range(9):
+        for j in range(9):
+            row_sudoku[i].count(j)
+            
