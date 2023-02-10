aa49f7b64d03264d0cffdda1d6eb330489b992bd
seoyoung81 <seosue0801@gmail.com>
2023-02-10 10:42:59 +0900

    col_arr modify

--- SWEA/D2/1974_sudoku.py
+++ SWEA/D2/1974_sudoku.py
@@ -3,6 +3,13 @@
 T = int(input())    # 테스트 케이스의 개수
 
 for tc in range(1, T+1):
-    sudoku = [list(map(int, input().split()) for _ in range(9)]     # 스도쿠를 2차원 리스트로 입력 받음
+    row_sudoku = [list(map(int, input().split())) for _ in range(9)]     # 스도쿠를 2차원 리스트로 입력 받음
+    col_sudoku = [[0] * 9 for _ in range(9)]
 
+    for i in range(9):
+        for j in range(9):
+            col_sudoku[i][j] = row_sudoku[j][i]
+
+    print(row_sudoku)
+    print(col_sudoku)
 
--- col_arr.md
+++ col_arr.md
@@ -9,3 +9,12 @@
         for j in range(8):
             col_arr[j] += i[j]
   ```
+
+```python
+row_sudoku = [list(map(int, input().split())) for _ in range(9)]     # 스도쿠를 2차원 리스트로 입력 받음
+    col_sudoku = [[0] * 9 for _ in range(9)]
+
+    for i in range(9):
+        for j in range(9):
+            col_sudoku[i][j] = row_sudoku[j][i]
+```
