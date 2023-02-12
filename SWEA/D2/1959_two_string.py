d4a1c9c31feb8026b37f5fb51992c89f9ec0bf1b
고서영 <seosue0801@gmail.com>
2023-02-12 23:58:02 +0900

    SWEA 1961 ing

---
+++ SWEA/D2/1959_two_string.py
@@ -0,0 +1,33 @@
+T = int(input())
+for test_case in range(1, T+1):
+    N, M = map(int, input().split())    # A의 개수, B의 개수
+    numbers_A = list(map(int, input().split()))
+    numbers_B = list(map(int, input().split()))
+
+    # 길이가 더 긴 걸 B에 주기
+    if len(numbers_B) < len(numbers_A):
+        numbers_B, numbers_A = numbers_A, numbers_B
+
+    n = len(numbers_A)
+    m = len(numbers_B)
+
+    # 곱하기 구하기
+    multiple_list = [0] * n
+    for j in range(n):
+        for i in range(n):
+            multiple_list[j] += (numbers_A[i]*numbers_B[i+j])
+    # print(multiple_list)
+
+    '''
+    a[0]*b[0] + a[1]*b[1] + a[2]*b[2]
+    a[0]*b[1] + a[1]*b[2] + a[2]*b[3]
+    a[0]*b[2] + a[1]*b[3] + a[2]*b[4]
+    '''
+    # 최댓값 구하기
+    maxV = multiple_list[0]
+    for i in range(1, n):
+        if maxV < multiple_list[i]:
+            maxV = multiple_list[i]
+
+    # 출력
+    print(f'#{test_case} {maxV}')
