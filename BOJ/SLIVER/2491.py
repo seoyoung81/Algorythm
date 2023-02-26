b7ada9a61e812c6077c6a25d98fb549457035528
고서영 <seosue0801@gmail.com>
2023-02-26 15:00:41 +0900

    2491 add

--- BOJ/SLIVER/2491.py
+++ BOJ/SLIVER/2491.py
@@ -1,20 +1,25 @@
 N = int(input())    # 수열의 길이
 numbers = list(map(int, input().split()))   # 수열
 
-cnt_list = [0] * N
+cnt_list = [[0] * (N+1), [0] * (N+1)]
+# print(cnt_list)
 
 
 for i in range(N-1):
-    if numbers[i] <= numbers[i+1]:  # 증가
-        cnt_list[i+1] += cnt_list[i] + 1
-        if numbers[i-1] >= numbers[i]:
-            cnt_list[i] = 0
-            cnt_list[i+1] += cnt_list[i] + 1
-    elif numbers[i] >= numbers[i+1]:   # 감소
-        cnt_list[i] = 1
-        cnt_list[i+1] += cnt_list[i] + 1
-        if numbers[i-1] <= numbers[i]:
-            cnt_list[i] = 0
-            cnt_list[i+1] += cnt_list[i] + 1
+    if numbers[i] < numbers[i+1]:  # 증가
+        cnt_list[0][i+1] += cnt_list[0][i] + 1  # 증가 정보만 담아서 count 세기
 
-print(max(cnt_list)-1)
+    elif numbers[i] > numbers[i+1]:   # 감소
+        cnt_list[1][i+1] += cnt_list[1][i] + 1  # 감소 정보만 담아서 count 세리
+
+    else:       # 변화가 없는 경우
+        cnt_list[0][i + 1] += cnt_list[0][i] + 1
+        cnt_list[1][i + 1] += cnt_list[1][i] + 1
+
+a = max(cnt_list[0])
+b = max(cnt_list[1])
+
+if a >= b:
+    print(a+1)
+else:
+    print(b+1)
