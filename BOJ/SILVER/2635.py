e07830e1af161b53a9e4dc68faef68a66110ab8e
고서영 <seosue0801@gmail.com>
2023-03-01 16:35:55 +0900

    2635

--- BOJ/SILVER/2635.py
+++ BOJ/SILVER/2635.py
@@ -1,10 +1,37 @@
 N = int(input())
 
+if N <= 2:
+    memo = [0] * (N*N*N+1)
+else:
+    memo = [0] * (N*2 + 1)
 
-for i in range(1, N):   # 두번째 수는 1부터 N-1 까지 중 하나라고 생각
-    memo = [0] * (N + 1)  # 모든 수를 저장할 memo 리스트
-    memo[0] = N  # 첫번째 수는 주어진 입력 값
-    memo[1] = i     # 두번째 수는 계속 반복
-    memo[i+1] = memo[i-1] - memo[i]
+memo[0] = N
 
-    print(memo)
+numbers = []
+# 두번째 숫자 정하기
+for i in range(N+1):
+    memo[1] = i
+    # 다음 숫자들 정하기
+    for j in range(2, len(memo)):
+        memo[j] = memo[j-2] - memo[j-1]
+
+        # 음수가 되는 순간
+        if memo[j] < 0:
+            numbers.append(memo[:j])
+            break
+
+# print(numbers)
+
+if N == 1:
+    numbers = [[1, 1, 0, 1]]
+
+# 가장 길이가 긴 리스트 찾기
+max_lst = numbers[0]
+for lst in numbers:
+    if len(max_lst) < len(lst): # 길이가 더 긴 리스트가 있다면
+        max_lst = lst
+
+print(len(max_lst))
+print(*max_lst)
+
+
