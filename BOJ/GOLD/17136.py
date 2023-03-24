63589cd93fae9cb51f700a1379043212fd8ebda9
seoyoung81 <seosue0801@gmail.com>
2023-03-24 23:08:58 +0900

    2992

--- BOJ/GOLD/17136.py
+++ BOJ/GOLD/17136.py
@@ -1,10 +1,25 @@
-arr = [list(map(int, input().split())) for _ in range(10)]
+arr = [list(map(str, input().split())) for _ in range(10)]
+paper = []
+rec = [0] * 6   # cnt
+# bruteforce
+for i in range(10):
+    paper.append(''.join(arr[i]))
 
 # 전체 탐색
 for i in range(10):
     for j in range(10):
-        if arr[i][j] == 1: # 색종이를 붙여야 하는 경우에
-            # 가로 검사
-            if i <= 5:
-                if arr[i+1][j] == 1:
-                    if arr[i + 1][j] == 1:
+        if arr[i][j] == 1:
+            count = 0
+            if j < 5:
+                if arr[i][j:j+5] == '11111':
+                    for k in range(1, 4):
+                        if arr[i+k][j:j+5] == '11111':
+                            count += 1
+                        else:
+                            print(k)
+                            break
+                    if count == 4:
+                        rec[5] += 1
+
+
+
