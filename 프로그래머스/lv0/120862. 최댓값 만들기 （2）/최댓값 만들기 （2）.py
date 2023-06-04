87629afa07127120aaef3481b17480ab5bff21ec
koko <122508505+seoyoung81@users.noreply.github.com>
2023-06-04 22:03:00 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
@@ -0,0 +1,7 @@
+def solution(numbers):
+    answer = -100000000
+    for i in range(len(numbers)):
+        for j in range(i+1, len(numbers)):
+            if numbers[i] * numbers[j] > answer:
+                answer = numbers[i] * numbers[j]
+    return answer
