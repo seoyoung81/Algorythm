b7bb2c66f2da1a8714f6229a4ff8a0a9be58789a
koko <122508505+seoyoung81@users.noreply.github.com>
2023-06-11 20:48:57 +0900

    [level 0] Title: 소인수분해, Time: 0.21 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.py
@@ -0,0 +1,9 @@
+def solution(n):
+    answer = []
+    
+    for i in range(2, n+1):
+        while n % i == 0:
+            if i not in answer:
+                answer.append(i)
+            n = n // i
+    return answer
