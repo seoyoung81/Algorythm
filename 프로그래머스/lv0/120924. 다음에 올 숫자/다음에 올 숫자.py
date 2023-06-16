6e7e6ac81f819f28f11c57cc900626f59bb835c4
koko <122508505+seoyoung81@users.noreply.github.com>
2023-06-16 23:58:36 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.py
@@ -0,0 +1,8 @@
+def solution(common):
+    answer = 0
+    for i in range(3):
+        if common[1] - common[0] == common[2] - common[1]:
+            answer = common[-1] + (common[1] - common[0])
+        elif common[1] // common[0] == common[2] // common[1]:
+            answer = common[-1] * (common[1] // common[0])
+    return answer
