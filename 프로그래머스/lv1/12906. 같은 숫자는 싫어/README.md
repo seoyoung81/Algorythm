6b5ad89c0a78519d8489dc94e9a15788ef452fc4
koko <122508505+seoyoung81@users.noreply.github.com>
2023-09-06 17:02:19 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 50.13 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/README.md
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 같은 숫자는 싫어 - 12906 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906?language=javascript) 
 
 ### 성능 요약
 
-메모리: 27.9 MB, 시간: 69.37 ms
+메모리: 83.3 MB, 시간: 50.13 ms
 
 ### 구분
 
---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,15 @@
+function solution(arr)
+{
+    var answer = [];
+    arr.forEach(num => {
+        if (!answer){
+            answer.push(num)
+        } else {
+            if (answer[answer.length - 1] !== num){
+                answer.push(num)
+            }
+        }
+    })
+    
+    return answer;
+}
