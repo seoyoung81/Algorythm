8e43540a0bdaed9c05229c97030bc6c32a8cb3be
고서영 <122508505+seoyoung81@users.noreply.github.com>
2023-12-27 00:25:20 +0900

    [Bronze II] Title: Farm, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/16283. Farm/Farm.py
+++ 백준/Bronze/16283. Farm/Farm.py
@@ -1,19 +1,15 @@
-# 양 a 그램, 염소 b 그램, 전체 n마리, 소비한 사료 w 그램
 a, b, n, w = map(int, input().split())
-
-result_sheep = 0
-result_goat = 0
+# 양 사료, 염소 사료, 전체 몇마리, 전체 사료
 result = 0
-
+sheep = 0
+goat = 0
 for i in range(1, n):
-    sheep = i
-    goat = n - i
-    if sheep * a + goat * b == w:
-        result_sheep = sheep
-        result_goat = goat
+    if a*i + b*(n-i) == w:
         result += 1
+        sheep = i
+        goat = n-i
 
-if result != 1:
-    print(-1)
+if result == 1:
+    print(sheep, goat)
 else:
-    print(result_sheep, result_goat)
+    print(-1)
