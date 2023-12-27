9242c57d87905b2e9337b7f275a11ca7deb11152
고서영 <122508505+seoyoung81@users.noreply.github.com>
2023-12-27 15:28:57 +0900

    [Bronze III] Title: 대회 or 인턴, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2875. 대회 or 인턴/대회 or 인턴.py
+++ 백준/Bronze/2875. 대회 or 인턴/대회 or 인턴.py
@@ -1,11 +1,12 @@
-N, M, K = map(int, input().split())
-# 여, 남, 반드시 인턴십에 참여해야 하는 학생
 
+N, M, K = map(int, input().split())
 team = 0
-while True :
+
+while True:
     N-=2
     M-=1
-    if N<0 or M<0 or (N+M)<K:
+    if N < 0 or M < 0 or (N+M) < K:
         break
-    team+=1
+    team += 1
+
 print(team)
