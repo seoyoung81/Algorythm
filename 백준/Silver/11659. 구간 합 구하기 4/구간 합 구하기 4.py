878ec857d1f318a76f0deec2d992fa9db9ccabae
고서영 <122508505+seoyoung81@users.noreply.github.com>
2024-01-07 23:06:11 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 4660 ms, Memory: 121740 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -0,0 +1,10 @@
+N, M = map(int, input().split())
+
+number = list(map(int, input().split()))
+sum = [0] * (N+1)
+for i in range(1, N+1):
+    sum[i] = sum[i-1] + number[i-1]
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    print(sum[b]-sum[a-1])
