5ef5ed3fddbb2f2bcfda3f3196208c8b3ac38fc5
고서영 <122508505+seoyoung81@users.noreply.github.com>
2024-01-12 18:03:55 +0900

    [Silver III] Title: 수열, Time: 140 ms, Memory: 122520 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -1,24 +1,16 @@
-N, K = map(int, input().split()) # 총 개수, 연속의 개수
-numbers = list(map(int, input().split()))
+import sys
+input = sys.stdin.readline
 
-# 슬라이딩 윈도우
-# 시간 초과
-# lst = numbers[:K]
-# total = [sum(lst)]
-# for i in range(K, N):
-#     lst.pop(0)          # 제일 앞에 꺼 빼고
-#     lst.append(numbers[i])      # 다음 꺼 더해주기
-#     total.append(sum(lst))
-#
-#
-# print(max(total))
+N, K = map(int, input().split())
+number = [0] + list(map(int, input().split()))
+number_sum = [0] * (N+1)
 
-total_lst = [sum(numbers[:K])]
-total = sum(numbers[:K])
-for i in range(N-K):
-    total -= numbers[i]
-    total += numbers[i+K]
-    total_lst.append(total)
+for i in range(1, N+1):
+    number_sum[i] = number_sum[i-1] + number[i]
 
-print(max(total_lst))
+result = min(number) * K
+for i in range(K, N+1):
+    if result < number_sum[i] - number_sum[i-K]:
+        result = number_sum[i] - number_sum[i-K]
 
+print(result)
