51f0d25857a67331ff3079b1cde5a3dda8e7289e
고서영 <122508505+seoyoung81@users.noreply.github.com>
2024-01-18 23:06:57 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 132 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,35 +1,23 @@
-height_list = []
+numbers = []
+for _ in range(9):
+    n = int(input())
+    numbers.append(n)
 
-for i in range(9):
-    h = int(input())
-    height_list.append(h)
-# print(height_list) # [20, 7, 23, 19, 10, 15, 25, 8, 13]
-sub = sum(height_list) - 100    # 40
+numbers.sort()
+# 7개의 숫자의 합이 100이 되는 것을 찾아 오름차순으로 출력하기
+sum_ = sum(numbers)
+sum_two = sum_ - 100
+s = 0
+e = 8
+while s < e:
+    if numbers[s] + numbers[e] == sum_two:
+        numbers.remove(numbers[e])
+        numbers.remove(numbers[s])
+        break
+    elif numbers[s] + numbers[e] < sum_two:
+        s += 1
+    else:
+        e -= 1
 
-# 2개 합 40 찾기 -> 36개
-first = 0   # 초기값 0 설정
-second = 0
-
-for i in range(9):
-    for j in range(i+1, 9): # first 보다 뒤에 있는 값이 필요
-        if height_list[i] + height_list[j] == sub:  # 더해서 40이면
-            first = height_list[i]      # first 에 i 할당
-            second = height_list[j]     # second 에 j 할당
-            break                       # 하나씩만 찾고 멈추기
-
-height_list.remove(first)       # first 리스트에서 삭제
-height_list.remove(second)     
-            
-# print(height_list)
-# new_height = ''.join([str(n) for n in height_list])
-
-height_list.sort()  # 오름차순으로 정렬하기
-
-for h in height_list:
-    print(h)
-
-
-
-
-            
-
+for i in range(7):
+    print(numbers[i])
