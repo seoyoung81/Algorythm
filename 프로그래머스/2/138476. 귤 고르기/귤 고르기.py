6820ccf0289d8f842e3a6f539345dd857b8a9336
Seoyoung Ko <122508505+seoyoung81@users.noreply.github.com>
2024-03-19 00:09:51 +0900

    [level 2] Title: 귤 고르기, Time: 27.43 ms, Memory: 21.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -0,0 +1,18 @@
+from collections import Counter
+
+def solution(k, tangerine):
+    answer = 0
+    counter=Counter(tangerine)
+    # {3: 2, 2: 2, 5: 2, 1: 1, 4: 1}
+    sort_=sorted(counter.items(),key=lambda x:x[1],reverse=True)
+
+    #정렬된 딕셔너리로 귤 개수 맞추기
+    cnt=0
+    for i in sort_:
+        k-=i[1]
+        answer+=1
+        
+        if k<=0:
+            break
+            
+    return answer
