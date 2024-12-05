3bda781e7ea995a3e3fdc97f44ca83474d0846f1
Seoyoung Ko <122508505+seoyoung81@users.noreply.github.com>
2024-12-06 00:02:58 +0900

    [Silver IV] Title: 설탕 배달, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,16 +1,14 @@
 N = int(input())
+lst = []
+r = N // 3
 
-# 5kg 봉지를 최대한 사용
-bags = 0
-while N >= 0:
-    # N이 5의 배수라면 (최대한 5kg 봉지를 사용)
-    if N % 5 == 0:
-        bags += N // 5
-        print(bags)
-        break
-    # 그렇지 않다면 3kg 봉지 하나 사용
-    N -= 3
-    bags += 1
+for i in range(r, -1, -1):
+    a = N - i * 3
+    b = a % 5
+    if b == 0:
+      lst.append(i + a//5)
+
+if lst:
+  print(min(lst))
 else:
-    # 정확히 N kg을 만들 수 없는 경우
-    print(-1)
+  print(-1)
